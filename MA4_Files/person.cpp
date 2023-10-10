0416b2e84a4c4d1342557a4405ba4874bcab1802
ericbjorfors <eric.bjorfors@telia.com>
2023-10-10 22:38:45 +0200

    Fix person.py code

--- MA4_Files/person.cpp
+++ MA4_Files/person.cpp
@@ -6,9 +6,10 @@ class Person{
 		Person(int);
 		int get();
 		void set(int);
-		int fib(int);
+		int fib();
 	private:
 		int age;
+		int _fib(int n);
 	};
  
 Person::Person(int n){
@@ -22,13 +23,16 @@ int Person::get(){
 void Person::set(int n){
 	age = n;
 	}
-int Person::fib(int n){
-	if(n <= 1){
-		return n;
-		}
-	else{
-		return(fib(n-1)+fib(n-2));		
-		}
+int Person::_fib(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return _fib(n - 1) + _fib(n - 2);
+    }
+}
+
+int Person::fib(){
+	return _fib(age);
 	}
 
 
@@ -36,7 +40,7 @@ extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
 	void Person_set(Person* person, int n) {person->set(n);}
-	int Person_fib(Person* person, int n) {return person->fib(n);}
+	int Person_fib(Person* person) {return person->fib();}
 	void Person_delete(Person* person){
 		if (person){
 			delete person;
